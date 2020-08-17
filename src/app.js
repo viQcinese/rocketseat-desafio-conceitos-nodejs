c667606a3dcaeb3fccf4e3a04f87c992b08894de
Vitor Senise <vitor.senise@gmail.com>
2020-08-17 18:20:30 -0300

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.send(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  request.body.id = uuid();
+  request.body.likes = 0;
+  repositories.push(request.body)
+  response.send(request.body)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repIndex = repositories.findIndex( rep => rep.id === id )
+
+  console.log(repIndex)
+
+  if (repIndex < 0) {
+    return response.status(400).json({
+      success: "false",
+      error: "Repository not found"
+    })
+  }
+
+  repositories[repIndex].title = title,
+  repositories[repIndex].url = url,
+  repositories[repIndex].techs = techs
+
+  return response.send(repositories[repIndex])
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repIndex = repositories.findIndex( rep => rep.id === id )
+
+  if (repIndex < 0) {
+    return response.status(400).json({
+      success: "false",
+      error: "Repository not found"
+    })
+  }
+
+  repositories.splice(repIndex, 1)
+
+  return response.status(204).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  
+  const repIndex = repositories.findIndex( rep => rep.id === id )
+
+  if (repIndex < 0) {
+    return response.status(400).json({
+      success: "false",
+      error: "Repository not found"
+    })
+  }
+
+  repositories[repIndex].likes += 1;
+
+  return response.send({likes: repositories[repIndex].likes})
+
 });
 
 module.exports = app;
